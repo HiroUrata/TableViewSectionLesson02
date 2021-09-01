a4eb8d73bf4a0759e8cdddb0fc0bb1003f8c469b
HiroUrata <1712026@i.softbank.jp>
2021-09-01 14:09:55 +0900

    カスタム前のTableView 追加

--- TableViewSectionLesson02/ViewController.swift
+++ TableViewSectionLesson02/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+       
     }
 
 
 }
 
+extension ViewController:UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+}
+
+
+extension ViewController:UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = {() -> String in
+            
+            if indexPath.row < 9{
+                
+                return "Cell0\(String(indexPath.row + 1))"
+                
+            }else{
+                
+                return "Cell\(String(indexPath.row + 1))"
+                
+            }
+            }()
+        
+        return cell
+    }
+    
+    
+    
+}
